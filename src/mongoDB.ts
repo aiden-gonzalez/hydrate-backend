0fd7f592279dff318c8b9cd82120b86678408751
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2023-02-20 12:20:01 -0500

    Add supporting functions to mongo connection

--- src/mongoDB.ts
+++ src/mongoDB.ts
@@ -1,10 +1,59 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
 const uri = process.env.MONGO_URL;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
+let client;
+let db;
+let bathRate;
+let bath;
+let fountRate;
+let fount;
+let pic;
+let user;
+
+function connect () {
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    client.connect(err => {
+        if (err) {
+            console.log(err);
+        }
+        db = client.db("hydRate");
+        bathRate = db.collection("Bathroom Ratings");
+        bath = db.collection("Bathrooms");
+        fountRate = db.collection("Fountain Ratings");
+        fount = db.collection("Fountains");
+        pic = db.collection("Pictures");
+        user = db.collection("Users");
+    });
+}
+
+function get() {
+    return db;
+}
+
+function bathroomRatings() {
+    return bathRate;
+}
+
+function bathrooms() {
+    return bath;
+}
+
+function fountainRatings() {
+    return fountRate;
+}
+
+function fountains() {
+    return fount;
+}
+
+function pictures() {
+    return pic;
+}
+
+function users() {
+    return user;
+}
+
+function close() {
     client.close();
-});
+}
