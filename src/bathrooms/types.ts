7f115a5f3c52478dc8d479e19876b69c9b32131e
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2024-04-28 18:16:22 -0500

    Switch to using path for parameters

--- src/bathrooms/types.ts
+++ src/bathrooms/types.ts
@@ -1,15 +1,8 @@
-import {components} from "../../schema";
+import {paths, components} from "../../schema";
 
 export type IBathroomInfo = components["schemas"]["BathroomInfo"];
 export type IBathroom = components["schemas"]["Bathroom"];
 export type IBathroomRatingDetails = components["schemas"]["BathroomRatingDetails"];
 export type IBathroomRating = components["schemas"]["BathroomRating"];
 
-export type IBathroomQueryParams = {
-  baby_changer?: boolean;
-  sanitary_products?: boolean;
-  gender?: string;
-  latitude?: number;
-  longitude?: number;
-  radius?: number;
-};
+export type IBathroomQueryParams = paths["/api/bathrooms"]["get"]["parameters"]["query"];
--- src/fountains/types.ts
+++ src/fountains/types.ts
@@ -1,13 +1,8 @@
-import {components} from "../../schema";
+import {paths, components} from "../../schema";
 
 export type IFountainInfo = components["schemas"]["FountainInfo"];
 export type IFountain = components["schemas"]["Fountain"];
 export type IFountainRatingDetails = components["schemas"]["FountainRatingDetails"];
 export type IFountainRating = components["schemas"]["FountainRating"];
 
-export type IFountainQueryParams = {
-  bottle_filler?: boolean;
-  latitude?: number;
-  longitude?: number;
-  radius?: number;
-};
+export type IFountainQueryParams = paths["/api/fountains"]["get"]["parameters"]["query"];
