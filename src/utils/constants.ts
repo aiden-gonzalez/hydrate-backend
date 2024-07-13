9a46a5c9babc38cba383b58d6cab3ceb2cbc9852
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2024-07-13 13:08:55 -0500

    Edit bathroom rating and fountain rating ID prefixes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -32,9 +32,9 @@ export const HASH_DIGEST  = "sha512";
 
 // ID prefixes
 export const BATHROOM_ID_PREFIX  = "bath";
-export const BATHROOM_RATING_ID_PREFIX  = "bath_rate";
+export const BATHROOM_RATING_ID_PREFIX  = "bathrate";
 export const FOUNTAIN_ID_PREFIX  = "fount";
-export const FOUNTAIN_RATING_ID_PREFIX  = "fount_rate";
+export const FOUNTAIN_RATING_ID_PREFIX  = "fountrate";
 export const USER_ID_PREFIX  = "user";
 export const PICTURE_ID_PREFIX  = "pic";
 
