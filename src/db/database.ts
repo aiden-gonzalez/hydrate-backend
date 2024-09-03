25c0a418bb81f102d14abe9eb4f753d7f19c0e32
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2024-09-02 19:27:50 -0500

    fixes for database connection

--- src/db/database.ts
+++ src/db/database.ts
@@ -3,13 +3,22 @@ import { Pool } from 'pg'
 import { Kysely, PostgresDialect } from 'kysely'
 
 const dialect = new PostgresDialect({
-  pool: new Pool({
-    database: 'postgres',
-    host: 'localhost',
-    user: 'postgres',
-    port: 5432,
-    max: 10
-  })
+  pool: process.env.NODE_ENV == "cloud" ?
+    new Pool({
+      database: process.env.CLOUD_PG_DB,
+      host: process.env.CLOUD_PG_HOST,
+      user: process.env.CLOUD_PG_USER,
+      password: process.env.CLOUD_PG_PASS,
+      port: 5432,
+      max: 10
+    }) : new Pool({
+      database: process.env.LOCAL_PG_DB,
+      host: process.env.LOCAL_PG_HOST,
+      user: process.env.LOCAL_PG_USER,
+      password: process.env.LOCAL_PG_PASS,
+      port: 5432,
+      max: 10
+    })
 });
 
 // Database interface is passed to Kysely's constructor, and from now on, Kysely
